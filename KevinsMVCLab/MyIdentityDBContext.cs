f95d229844b2076ff44ab9ca2992bec8559974ac
KevinKarlberg <Kevinkarlberg@hotmail.se>
2017-02-17 19:39:06 +0100

    Added migration and identity to new project, bit left to do with solving glitches and connecting the two databases

---
+++ KevinsMVCLab/MyIdentityDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KevinsMVCLab
+{
+    public class MyIdentityDBContext : IdentityDbContext<IdentityUser>
+    {
+        public MyIdentityDBContext() : base("name = DefaultConnection") {
+        }
+    }
+}
