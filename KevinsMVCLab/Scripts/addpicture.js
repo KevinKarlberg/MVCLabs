61d08fb052686367c60f46028d21974a7b7e3010
KevinKarlberg <Kevinkarlberg@hotmail.se>
2017-02-18 06:14:26 +0100

    Added the js files

---
+++ KevinsMVCLab/Scripts/addpicture.js
@@ -0,0 +1,64 @@
+﻿var form = $('#createform');
+
+form.submit(function (e) {
+
+    e.preventDefault();
+    if (form.valid()) {
+        swal({
+            title: "Image upload",
+            text: "Submit to upload images",
+            type: "info",
+            showCancelButton: true,
+            closeOnConfirm: false,
+            showLoaderOnConfirm: true,
+        },
+            function () {
+                $.ajax({
+                    method: "POST",
+                    url: "/Picture/Create",
+
+                    data: new FormData(form[0]),
+                    success: function (data) {
+                        if (data.match("^Added")) {
+                            uploadMoreImagesPrompt();
+                        }
+                        else {
+                            swal("Oh no!", data, "error")
+                        }
+                    },
+                    error: function (e) {
+                        swal("Oh no!", "Something went wrong at the server!", "error")
+                    },
+                    processData: false,
+                    contentType: false
+                });
+            });
+
+    }
+});
+
+var uploadMoreImagesPrompt = function () {
+
+    swal({
+        title: "Success!?",
+        text: "Upload more pictures?",
+        type: "success",
+        showCancelButton: true,
+        confirmButtonColor: "#779b79",
+        confirmButtonText: "Yes",
+        cancelButtonText: "No",
+        closeOnConfirm: true,
+        closeOnCancel: true
+    },
+function (isConfirm) {
+    if (isConfirm) {
+        form[0].reset();
+    } else {
+        window.location = $('#backbtn').attr('href');
+    }
+});
+
+
+
+
+};
---
+++ KevinsMVCLab/Scripts/deletepicture.js
@@ -0,0 +1,32 @@
+﻿var deleteform = $('#deletepictureform');
+
+deleteform.on('submit', function (e) {
+    e.preventDefault();
+    swal({
+        title: "Delete Image!?",
+        text: "Submit to delete image.",
+        type: "warning",
+        showCancelButton: true,
+        closeOnConfirm: false,
+        showLoaderOnConfirm: true,
+    },
+        function () {
+            $.ajax({
+                url: "/Picture/Delete",
+                type: "post",
+                data: deleteform.serialize(),
+                success: function (data) {
+                    swal("Success", "The Image has been deleted.", "success");
+                    window.location.href = data;
+
+                },
+                error: function (e) {
+                    swal("Error", "Couldn't Delete the Image", "error");
+                }
+            });
+
+
+            //});
+
+        });
+});
